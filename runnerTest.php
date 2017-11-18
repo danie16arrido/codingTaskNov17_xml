0335eb35514fd7194bd0c703d15033cf3ae36c6a
danie1garrido <daniel.garrido.info@gmail.com>
2017-11-18 22:42:00 +0000

    unify vallidator inputs

--- runnerTest.php
+++ runnerTest.php
@@ -5,26 +5,13 @@ include "validator.php";
 
 $myFile = "stores.xml";
 $myParser = new XmlParser;
-// $myParser->setSourceFile("stores.xml");
-$myParser->setFieldsRequired ([
-    "number",
-    "name",
-    "address",
-    "siteid",
-    "lat",
-    "lon",
-    "phone_number",
-    "cfs_flag"
-]);
-$stores = $myParser->parseData($myFile);
 
-$myValidator = new validator;
 $validatorValues = [
     "number" => "/^[0-9]{2,3}$/",
     "name" => "/\p{L}/",
     "siteid" => "/^[A-Z]{2}$/",
     "address_line_1" => "/\p{L}/",
-    "address_line_2" => "/\p{L}/",
+    "address_line_2" => "/^(?!\s*$).+/",
     "address_line_3" => "/\p{L}/",
     "city" => "/\p{L}/",
     "county" => "/^[a-z\d\-_\s]+$/i",
@@ -35,13 +22,21 @@ $validatorValues = [
     // "phone_number1" => "/^[0-9]{4} [0-9]{3} [0-9]{3,4}$/",
     "cfs_flag" => "/^(?:Y|N|y|n)$/"
 ];
+$myParser->setFieldsRequired(array_keys($validatorValues));
+$stores = $myParser->parseData($myFile);
+
+$myValidator = new validator;
 $myValidator->setValidators($validatorValues);
 $myValidator->validateData($stores);
+
 //  echo $myValidator->getDestinationFile();
 // $loader = new dbloader;
 // $loader->setConnectioData(db, user, password);
 // $myfile =  $myValidator->getDestinationFile();
 // $loader->loadToDB($myfile);
+
+
+
 
 
 
